de2fff393597021d4def68b422f0c2ae101ac527
unknown <gustavozenke01@gmail.com>
2021-01-04 22:40:10 -0300

    Teste

--- Introdução à Programação Orientada a Objetos/Aluno/src/application/Program.java
+++ Introdução à Programação Orientada a Objetos/Aluno/src/application/Program.java
@@ -8,7 +8,7 @@
 	(primeiro trimestre vale 30 e o segundo e terceiro valem 35 cada). Ao final, mostrar qual a nota final do aluno no
 	ano. Dizer também se o aluno está aprovado (PASS) ou não (FAILED) e, em caso negativo, quantos pontos faltam
 	para o aluno obter o mínimo para ser aprovado (que é 60% da nota). Você deve criar uma classe Student para
-	resolver este problema.
+	resolver este problema..
 */
 
 package application;
